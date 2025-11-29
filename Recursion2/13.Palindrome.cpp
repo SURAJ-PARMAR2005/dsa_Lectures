5a216672982f1c01f025a7721cb2617c44896cb5
SURAJ-PARMAR2005 <golurajput87080@gmail.comgit config --list>
2025-11-29 21:30:36 +0530

    palindrome by rec

---
+++ Recursion2/13.Palindrome.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+bool isPal(string s , int i, int j){
+    if(i>j){
+        return true;
+    }
+    if(s[i] != s[j]) return false;
+    else return isPal(s,i+1,j-1);
+}
+int main() {
+    string s;
+    cin>>s;
+    cout<<isPal(s,0,s.length()-1);
+}
