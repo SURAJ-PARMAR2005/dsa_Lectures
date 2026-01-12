1e3f5dbe05d03fb2fbcc8251a8ecc7bad03369b1
SURAJ-PARMAR2005 <golurajput87080@gmail.com>
2026-01-12 22:14:22 +0530

    Refactor Helper.cpp to remove unused code and implement distance calculation

--- Leetcode/Helper.cpp
+++ Leetcode/Helper.cpp
@@ -1,18 +1,16 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<math.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
 using namespace std;
-int main() {
+int main()
+{
 
-    vector <string> str = {"abc","cae","cbe"};
-    for(int i  = 0;i<3;i++){
-        cout<<str[i]<<" ";
-    }
-    cout<<endl;
-    sort(str.begin(),str.end());
-     for(int i  = 0;i<3;i++){
-        cout<<str[i]<<" ";
-    }
-
+    int x1 = 3;
+    int y1 = 4; 
+    int x2 = -1;
+    int y2 =  0;
+     int dist = (y2-y1)*(y2-y1) + (x2-x1)*(x2-x1);
+    int shortDist = sqrt(dist);
+    cout<<shortDist<<endl;
 }
