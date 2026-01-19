0dba997bf67fa9dc580cd344142a4b0ccd20a48d
SURAJ-PARMAR2005 <golurajput87080@gmail.com>
2026-01-19 22:43:00 +0530

    quick Sort code

--- QuickSort/01BasicQuickSort.cpp
+++ QuickSort/01BasicQuickSort.cpp
@@ -1,10 +1,38 @@
 #include<iostream>
 using namespace std;
-void quickSort(int arr[],int si,int ei){
-    //5,1,8,2,7,6,3,4
-    int pivotIdx = partition(arr,si,ei);
-    //4,1,3,2,5,7,8,6      
+int partition(int arr[],int si,int ei){
+    int pivot  = arr[si];
+    int count = 0;
+    for(int i = si + 1;i<=ei;i++){
+        if(arr[i] <= pivot) count++;
+    }
+    int pivotIdx = count + si;
+    swap(arr[si],arr[pivotIdx]);
+    int i = si;
+    int j = ei;
+    while(i<pivotIdx || j>pivotIdx){
+      
+        if(arr[i] <=pivot) i++;
+        if(arr[j] >pivot) j--;
+        
 
+        // if(i>j) break;
+          else if(arr[i] >pivot && arr[j] <= pivot){
+          swap(arr[i],arr[j]);
+            i++;
+            j--;
+        }
+    }
+    
+
+}
+void quickSort(int arr[],int si,int ei){
+    if(si >= ei) return;
+    //5,1,8,2,7,6,3,4
+    int patIdx = partition(arr,si,ei);
+    //4,1,3,2,5,7,8,6      
+    quickSort(arr,si,patIdx-1);
+    quickSort(arr,patIdx+1,ei);
 }
 int main() {
     int arr[] = {5,1,8,2,7,6,3,4};
@@ -12,5 +40,9 @@ int main() {
     for(int i = 0;i<n;i++){
         cout<<arr[i]<<" ";
     }
+    cout<<endl;
     quickSort(arr,0,n-1);
+     for(int i = 0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
 }
