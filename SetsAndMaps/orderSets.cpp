f13efbfdd640002dbe04228ecb71bca5628b3d55
SURAJ-PARMAR2005 <golurajput87080@gmail.com>
2026-01-23 23:48:04 +0530

    partionQuestion prefix sum

--- SetsAndMaps/orderSets.cpp
+++ SetsAndMaps/orderSets.cpp
@@ -4,32 +4,32 @@
 using namespace std;
 int main() {
     //declare 
-    // set<int> s;
-    // s.insert(5);
-    // s.insert(4);
-    // s.insert(3);
-    // s.insert(2);
-
-    // //mehtods takes O(logn) time complexity
-
-    // for(int ele : s){
-    //     cout<<ele<<" ";
-    // }
-
-
-      map<string,int> mp;
-    mp["suraj"]  = 30;
-    mp["sanchit"]  = 10;
-    mp["vishwesh"]  = 20;
-
-//sorting happens on key
+    set<int> s;
+    s.insert(5);
+    s.insert(4);
+    s.insert(3);
+    s.insert(2);
 
     //mehtods takes O(logn) time complexity
 
-    for(auto ele : mp){
-        cout<<ele.first<<" ";
-        cout<<ele.second<<" ";
+    for(int ele : s){
+        cout<<ele<<" ";
     }
+
+
+//       map<string,int> mp;
+//     mp["suraj"]  = 30;
+//     mp["sanchit"]  = 10;
+//     mp["vishwesh"]  = 20;
+
+// //sorting happens on key
+
+//     //mehtods takes O(logn) time complexity
+
+//     for(auto ele : mp){
+//         cout<<ele.first<<" ";
+//         cout<<ele.second<<" ";
+//     }
 
 
 }
