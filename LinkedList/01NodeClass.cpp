88fb7f6f30c00d0b19355e4503a357d551b24f5a
SURAJ-PARMAR2005 <golurajput87080@gmail.com>
2026-02-08 22:57:59 +0530

    Linked List

--- LinkedList/01NodeClass.cpp
+++ LinkedList/01NodeClass.cpp
@@ -10,31 +10,71 @@ class Node{ //LL Node
     }
 };
 
-int main(){
-    Node a(10);
-    Node b(20);
-    Node c(30);
-    Node d(40);
-
-    //forming LL
-
-
-    a.next = &b;
-    b.next = &c;
-    c.next = &d;
-    d.next = NULL;
-    // a.next -> val = 60;
-    // cout<<(a.next)->val;
-    // cout<<(b.next)->val;
-
-    //a se d ki value kese print kraye
-    // cout<<((a.next)->next)->val;
-    
-    Node temp = a;//it have the content of node a but diff address of its own
-    while(1){
-        cout<<temp.val<<" ";
-        if(temp.next == NULL) break;
-        temp = *(temp.next);
+void display(Node * head){
+    Node * temp = head;
+    while(temp != NULL){
+        cout<<temp->val<<" ";
+        temp = temp->next;
     }
+    cout<<endl;
+}
 
+int size(Node * head){
+    Node * temp = head;
+    int n = 0;
+    while(temp != NULL){
+        n++;
+        temp = temp->next;
+    }
+   return n;
+}
+
+int main(){
+    // Node a(10);
+    // Node b(20);
+    // Node c(30);
+    // Node d(40);
+
+    // //forming LL
+
+
+    // a.next = &b;
+    // b.next = &c;
+    // c.next = &d;
+    // d.next = NULL;
+    // // a.next -> val = 60;
+    // // cout<<(a.next)->val;
+    // // cout<<(b.next)->val;
+
+    // //a se d ki value kese print kraye
+    // // cout<<((a.next)->next)->val;
+    
+    // Node temp = a;//it have the content of node a but diff address of its own
+    // while(1){
+    //     cout<<temp.val<<" ";
+    //     if(temp.next == NULL) break;
+    //     temp = *(temp.next);
+    // }
+
+    Node *a = new Node(10);//a stores the adress of a node
+    Node *b = new Node(20);
+    Node *c = new Node(30);
+    Node *d = new Node(40);
+    Node * e = new Node(50);
+
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = e;
+
+    // Node * temp = a;
+    // while(temp !=NULL){
+    //     cout<<temp->val<<" ";
+    //     temp = temp->next;
+    // }
+
+    // cout<<a->next->val;
+
+    display(a);
+    cout<<size(a);
 }
