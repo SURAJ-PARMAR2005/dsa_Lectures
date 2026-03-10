ccb0356c038de94796ea4dc5c20d8eaaf5d59aaa
SURAJ-PARMAR2005 <golurajput87080@gmail.com>
2026-03-10 23:22:54 +0530

    Add basic stack operations example in C++
    - Implemented a simple C++ program demonstrating stack operations using the STL stack.
    - Included functionality to push elements onto the stack and then pop them into a temporary stack before printing.
    - The program showcases the size of the stack and the order of elements after reversing them.

---
+++ Stack1/01.basics.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main() {
+    stack<int> st;
+    cout<<st.size()<<endl;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+    st.push(6);
+
+    stack<int> temp;
+
+    while(st.size() >0){
+        // cout<<st.top()<<" ";
+        int x = st.top();
+        st.pop();
+        temp.push(x);
+    }
+    
+    while(temp.size() > 0){
+        int x = temp.top();
+        cout<<x<<" ";
+        st.push(x);
+        temp.pop(); 
+    }
+
+}
