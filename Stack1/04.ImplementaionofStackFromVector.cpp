b682b269269cd4561fe9cbee5621bf9a42a3a2c9
SURAJ-PARMAR2005 <golurajput87080@gmail.com>
2026-03-15 22:11:38 +0530

    Implement stack functionality using a fixed-size array in C++
    - Added a Stack class with methods for push, pop, top, and size.
    - Implemented a simple main function to demonstrate stack operations.
    - Included error handling for stack overflow.

---
+++ Stack1/04.ImplementaionofStackFromVector.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+class Stack{
+  public:  
+    int arr[5];
+    int idx;
+    Stack(){
+        idx = -1;
+    }
+    void push(int val){
+        if(idx == 4){
+            cout<<"stack is full"<<endl;
+            return;
+        }
+        idx++;
+        arr[idx]  = val;
+    }
+    void pop() {
+        idx--;
+    }
+    int top(){
+        return arr[idx];
+    }
+    int size() {
+        return idx+1;
+    }
+};
+int main() {
+    Stack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(50);
+    st.push(60);
+
+
+    cout<<st.size()<<endl;
+    st.pop();
+    cout<<st.size()<<endl;
+    cout<<st.top()<<endl;
+    
+}
