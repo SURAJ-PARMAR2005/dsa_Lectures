3124d4b2921b1b44bd2f8d85eee8d4748cbfe2c2
SURAJ-PARMAR2005 <golurajput87080@gmail.com>
2026-04-21 18:31:52 +0530

    Refactor N-Queens solution to store valid configurations and return results

--- backtracking/3.NQueens.cpp
+++ backtracking/3.NQueens.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 vector<vector<char>> grid;
 
+vector<vector<string> > result;
 
 bool canPlaceQueen(int row , int col,int n){
     // column check 
@@ -26,12 +27,15 @@ bool canPlaceQueen(int row , int col,int n){
 void f(int row,int n){
     if(row == n){
         //we got one possible ans
+        vector<string>  temp;
         for(int i = 0;i<n;i++){
+        string res = "";
             for(int j = 0;j<n;j++){
-                cout<<grid[i][j]<<" ";
+                res+=grid[i][j];
             }
-            cout<<endl;
+          temp.push_back(res);
         }
+        return;
     }
 
     for(int col = 0;col<n;col++){
@@ -47,8 +51,11 @@ void f(int row,int n){
 }
 
 vector<vector<string> > nqueens(int n) {
+    grid.clear();
+    result.clear();
     grid.resize(n,vector<char> (n,'.'));
     f(0,n);
+    return result;
 }
 
 int main() {
