d65706b79dfbbe4c32212ace407a39ceed465c1e
SURAJ-PARMAR2005 <golurajput87080@gmail.com>
2026-05-05 22:15:40 +0530

    Add FloodFill function implementing depth-first search for color filling

---
+++ Graph/FloodFill.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+        //this func will do dfs from currRow, currCol, to its neighbours containng intial color 
+    void dfs(vector<vector<int>>&image , int currRow, int currCol, int intialColor,int newColor ){
+        int n = image.size();
+        int m = image[0].size();
+        if(currRow<0 || currCol<0 || currRow>=n || currCol>=m){
+            return; 
+        }
+        //if cell landed is not intialcolr
+        if(image[currRow][currCol] != intialColor){
+            return;
+        }
+        image[currRow][currCol] = newColor;
+        dfs(image,currRow+1,currCol,intialColor,newColor);
+        dfs(image,currRow,currCol-1,intialColor,newColor);
+        dfs(image,currRow-1,currCol,intialColor,newColor);
+        dfs(image,currRow,currCol+1,intialColor,newColor);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int initalColor = image[sr][sc];
+        if(color == initalColor) return image;
+        dfs(image,sr,sc,initalColor,color);
+        return image;
+    }
+};
